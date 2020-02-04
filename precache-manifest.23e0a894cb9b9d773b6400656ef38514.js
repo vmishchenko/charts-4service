b8a26a3a1f8511a7ee60922ea0c1ee2c2b107ba3
vmishchenko <vladislav.a.mishchenko@gmail.com>
2020-02-04 20:07:05 +0200

    Updates

---
+++ precache-manifest.23e0a894cb9b9d773b6400656ef38514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200f1d43723f7682353f773d548b5787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49376d57e3664a77a7eb",
+    "url": "/static/css/main.0456e41f.chunk.css"
+  },
+  {
+    "revision": "923d7a57f0b8d237c471",
+    "url": "/static/js/2.a8040e35.chunk.js"
+  },
+  {
+    "revision": "a22a7ae5fe503af7d2f4bea67c7372bc",
+    "url": "/static/js/2.a8040e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49376d57e3664a77a7eb",
+    "url": "/static/js/main.e064dfb3.chunk.js"
+  },
+  {
+    "revision": "133fa8ae83a6c13dba16",
+    "url": "/static/js/runtime-main.af288c70.js"
+  }
+]);
